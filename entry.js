12d6123752f464e7085ca117f9407726cdefd1bb
DreamerKing <dreamingking@live.cn>
2016-06-07 18:26:04 +0800

    add .editorconfig

--- entry.js
+++ entry.js
@@ -1,5 +1,5 @@
 // document.write("It works.");
-//require("!style!css!./style.css");
+require("!style!css!./style.css");
 //reuire('./content.js')
-require("./style.css");
+//require("./style.css");
 document.write(require("./content.js"));
